82cc3ecc06d13cbb1166e055bece8c282d7774be
at-phuongpham <phuong.pham@asiantech.vn>
2017-05-18 13:53:00 +0700

    input code of tourist

--- app/src/main/java/com/example/asiantech/travelapp/activities/RuleActivity.java
+++ app/src/main/java/com/example/asiantech/travelapp/activities/RuleActivity.java
@@ -1,17 +1,25 @@
 package com.example.asiantech.travelapp.activities;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.example.asiantech.travelapp.R;
 import com.example.asiantech.travelapp.activities.utils.Constant;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 /**
  * Created by asiantech on 11/03/2017.
@@ -44,10 +52,59 @@ public class RuleActivity extends AppCompatActivity {
                         intent = new Intent(RuleActivity.this, LoginTourGuideActivity.class);
                     }
                 } else {
+                    //show dialog de nhap ma code
+
                     intent = new Intent(RuleActivity.this, LoginTourRistActivity.class);
                 }
                 startActivity(intent);
             }
         });
     }
+
+    public void showInputCode(){
+        final Dialog dialog = new Dialog(RuleActivity.this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_input_code);
+        final EditText edtCode = (EditText) dialog.findViewById(R.id.edtPrice);
+        edtCode.requestFocus();
+
+        Button btnOk = (Button) dialog.findViewById(R.id.tvBtnOk);
+        Button btnCancel = (Button) dialog.findViewById(R.id.btnCancel);
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(edtCode.getText().toString().equals("")){
+                    Toast.makeText(RuleActivity.this,"Vui lòng nhập mã code",Toast.LENGTH_SHORT).show();
+                }
+                else{
+
+                }
+            }
+        });
+
+    }
+
+    public void checkCode(String code){
+        Firebase firebaseUser = new Firebase(getString(R.string.URL_BASE)+"/users");
+        firebaseUser.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+    }
+
 }
